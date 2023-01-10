1f5a5a1c299a2095aa4401ac147fb51f564807ea
Wayne Simmerson <wsimmerson@gmail.com>
2023-01-10 12:40:31 -0500

    change to mysql.  create booking and menu models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    number_of_guests = models.IntegerField(default=1)
+    booking_date = models.DateField()
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()

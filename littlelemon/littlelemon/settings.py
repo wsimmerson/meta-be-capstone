dbb8f066af19200aab13a5804a6e98489b7fd6f8
Wayne Simmerson <wsimmerson@gmail.com>
2023-01-10 14:59:01 -0500

    menu api

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'restaurant'
 ]
 
@@ -134,3 +135,13 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Menu
+from .serializers import MenuSerializer
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
